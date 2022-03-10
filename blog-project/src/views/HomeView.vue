b00ea672d8c9baa6ec901a95a7bbeac7a4dff3b3
Alex <aelmore@merkleinc.com>
2022-03-10 11:20:38 -0600

    Practice fetch requests using Vue Composition API

--- blog-project/src/views/HomeView.vue
+++ blog-project/src/views/HomeView.vue
@@ -1,83 +1,43 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-
-    <h2>Ninja One</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update Ninja One</button>
-
-    <h2>Ninja Two</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">Update Ninja Two</button>
-    <p>Ninjas combined age is {{ combinedAge }}</p>
-    <input type="text" v-model="search" />
-    <p>search Term: {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">
-      <p>{{ name }}</p>
-    </div>
-    <button @click="stopWatching">Stop watching</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length"><PostList :posts="posts" /></div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { ref, computed, watch, watchEffect } from "vue";
+import PostList from "../components/ PostList.vue";
+import { ref } from "vue";
 export default {
   name: "HomeView",
+  components: {
+    PostList,
+  },
   setup() {
-    // Refs inside the composition API's setup method
-    const ninjaOne = ref({ name: "Mario", age: 30 });
-    const ninjaTwo = ref({ name: "Luigi", age: 35 });
+    const posts = ref([]);
+    const error = ref(null);
 
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40;
+    const load = async () => {
+      try {
+        const post = await fetch(`http://localhost:3000/posts`);
+        if (post.status === 200 && post.ok) {
+          const json = await post.json();
+          const data = await json;
+          posts.value = data;
+        } else {
+          throw new Error("Unable to fetch the data");
+        }
+      } catch (err) {
+        error.value = err.message;
+      }
     };
-
-    const updateNinjaTwo = () => {
-      ninjaTwo.value.age = 45;
-    };
-
-    const search = ref("");
-
-    // Computed property inside the composition API's setup method
-    const combinedAge = computed(() => {
-      return ninjaOne.value.age + ninjaTwo.value.age;
-    });
-
-    const names = ref(["Mario", "Luigi", "Peach", "Toad", "Bowser", "Koopa"]);
-
-    // Watch hook inside the composition API's setup method
-    const stopWatch = watch(search, () => {
-      console.log("somebodys watching you!");
-    });
-
-    // WatchEffect hook inside the composition API's setup method
-    const stopeWatchEffect = watchEffect(() => {
-      console.log("watch effect function ran");
-    });
-
-    // Another Computed property inside the composition API's setup method
-    const matchingNames = computed(() => {
-      return names.value.filter((name) =>
-        name.toLowerCase().includes(search.value.toLowerCase())
-      );
-    });
-
-    // Function that stops watchers
-    const stopWatching = () => {
-      stopWatch();
-      stopeWatchEffect();
-    };
+    load();
     return {
-      ninjaOne,
-      updateNinjaOne,
-      ninjaTwo,
-      updateNinjaTwo,
-      combinedAge,
-      names,
-      search,
-      matchingNames,
-      stopWatching,
+      posts,
+      error,
     };
   },
 };
