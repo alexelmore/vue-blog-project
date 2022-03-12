71980640a2d240509addba3d4b427b52ec859c73
aelmore49 <alexelmore49@gmail.com>
2022-03-12 12:43:26 -0600

    Firebase DB Setup

---
+++ blog-project/src/firebase/config.js
@@ -0,0 +1,20 @@
+import { def } from "@vue/shared";
+import firebase from "firebase/app";
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDLtLK4mvOpjBYl1-lafK8_D_G_f8N4HNA",
+    authDomain: "vue-blogger-63e5d.firebaseapp.com",
+    projectId: "vue-blogger-63e5d",
+    storageBucket: "vue-blogger-63e5d.appspot.com",
+    messagingSenderId: "779401570996",
+    appId: "1:779401570996:web:783eb4209a80e4e46f45e1"
+};
+
+// Init firebase
+firebase.initializeApp(firebaseConfig);
+
+// Init firestore service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
