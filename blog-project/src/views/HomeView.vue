9f0b20b7c2b5ac8ada257bc9e2000f0c3d9d8529
aelmore49 <alexelmore49@gmail.com>
2022-03-12 12:28:04 -0600

    Style updates

--- blog-project/src/views/HomeView.vue
+++ blog-project/src/views/HomeView.vue
@@ -3,7 +3,7 @@
 		<h1>Home</h1>
 
 		<div v-if="error">{{ error }}</div>
-		<div v-if="posts.length">
+		<div v-if="posts.length" class="layout">
 			<PostList :posts="posts" />
 			<TagCloud :posts="posts" />
 		</div>
@@ -40,5 +40,11 @@ export default {
 	max-width: 1200px;
 	margin: 0 auto;
 	padding: 10px;
+}
+
+.layout {
+	display: grid;
+	grid-template-columns: 3fr 1fr;
+	gap: 20px;
 }
 </style>
