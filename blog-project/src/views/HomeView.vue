59464675a8b6f2fb9bf322850635cd512c339c34
aelmore49 <alexelmore49@gmail.com>
2022-03-12 08:24:59 -0600

    Setup for TagCloud Component

--- blog-project/src/views/HomeView.vue
+++ blog-project/src/views/HomeView.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <div v-if="error">{{ error }}</div>
-    <div v-if="posts.length"><PostList :posts="posts" /></div>
-    <div v-else><Spinner /></div>
-  </div>
+	<div class="home">
+		<h1>Home</h1>
+
+		<div v-if="error">{{ error }}</div>
+		<div v-if="posts.length">
+			<PostList :posts="posts" />
+			<TagCloud :posts="posts" />
+		</div>
+		<div v-else><Spinner /></div>
+	</div>
 </template>
 
 <script>
@@ -12,27 +16,29 @@
 import PostList from "../components/PostList.vue";
 import getPosts from "../composables/getPosts";
 import Spinner from "../components/Spinner.vue";
+import TagCloud from "../components/TagCloud.vue";
 export default {
-  name: "HomeView",
-  components: {
-    PostList,
-    Spinner,
-  },
-  setup() {
-    const { posts, error, load } = getPosts();
-    load();
-    return {
-      posts,
-      error,
-    };
-  },
+	name: "HomeView",
+	components: {
+		PostList,
+		Spinner,
+		TagCloud,
+	},
+	setup() {
+		const { posts, error, load } = getPosts();
+		load();
+		return {
+			posts,
+			error,
+		};
+	},
 };
 </script>
 
 <style>
 .home {
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 10px;
+	max-width: 1200px;
+	margin: 0 auto;
+	padding: 10px;
 }
 </style>
