7404ea0e9b35e966d6aefa2ea1258b2cfcec4694
aelmore49 <alexelmore49@gmail.com>
2022-03-12 13:42:46 -0600

    Setup for adding a single post to our Firestore DB

--- blog-project/src/views/Create.vue
+++ blog-project/src/views/Create.vue
@@ -1,140 +1,126 @@
 <template>
-  <div class="create">
-    <form @submit.prevent="handleSubmit">
-      <label>Title: </label>
-      <input required type="text" v-model="title" />
-      <label>Content: </label><textarea required v-model="body"></textarea>
-      <label>Tags (click enter to add a tag)</label>
-      <input v-model="tag" @keydown.enter.prevent="handleKeyDown" />
-      <button>Add Post</button>
-      <div v-for="tag in tags" :key="tag" class="pill">#{{ tag }}</div>
-    </form>
-    <div v-if="error">{{ error }}</div>
-  </div>
+	<div class="create">
+		<form @submit.prevent="handleSubmit">
+			<label>Title: </label>
+			<input required type="text" v-model="title" />
+			<label>Content: </label><textarea required v-model="body"></textarea>
+			<label>Tags (click enter to add a tag)</label>
+			<input v-model="tag" @keydown.enter.prevent="handleKeyDown" />
+			<button>Add Post</button>
+			<div v-for="tag in tags" :key="tag" class="pill">#{{ tag }}</div>
+		</form>
+		<div v-if="error">{{ error }}</div>
+	</div>
 </template>
 
 <script>
-import { ref } from "@vue/reactivity";
+import { ref } from "vue";
+import { projectFirestore } from "../firebase/config";
 import { useRouter } from "vue-router";
 export default {
-  name: "Create",
-  setup() {
-    // Ref for data binding to form inputs
-    const title = ref("");
-    const body = ref("");
-    const tags = ref([]);
-    const tag = ref("");
-    const error = ref(null);
+	name: "Create",
+	setup() {
+		// Ref for data binding to form inputs
+		const title = ref("");
+		const body = ref("");
+		const tags = ref([]);
+		const tag = ref("");
+		const error = ref(null);
 
-    // Init router for redirecting user to the home page after a successful POST request
-    const router = useRouter();
+		// Init router for redirecting user to the home page after a successful POST request
+		const router = useRouter();
 
-    // Url to use for POST fetch request
-    const url = "http://localhost:3000/posts";
+		// Function that handles adding new tags to the tags array
+		const handleKeyDown = () => {
+			if (!tags.value.includes(tag.value)) {
+				tags.value.push(tag.value.replace(/\s/, ""));
+			}
+			tag.value = "";
+		};
 
-    // Function that handles adding new tags to the tags array
-    const handleKeyDown = () => {
-      if (!tags.value.includes(tag.value)) {
-        tags.value.push(tag.value.replace(/\s/, ""));
-      }
-      tag.value = "";
-    };
+		// Function that handles form submission
+		const handleSubmit = async () => {
+			// Object that will be sent as the body for our POST Fetch Request
+			let newPost = {
+				title: title.value,
+				body: body.value,
+				tags: tags.value,
+			};
+			const res = await projectFirestore.collection("posts").add(newPost);
+			router.push("/");
+		};
 
-    // Function that handles form submission
-    const handleSubmit = async () => {
-      try {
-        // Object that will be sent as the body for our POST Fetch Request
-        let newPost = {
-          title: title.value,
-          body: body.value,
-          tags: tags.value,
-        };
-        // Make a post request
-        const request = await fetch(url, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(newPost),
-        });
-        if (request.status === 201 && request.ok) {
-          router.push("/");
-        } else {
-          throw new Error("Error: Unable to add your post");
-        }
-      } catch (err) {
-        error.value = "Error: Unable to add your post at this time";
-      }
-    };
-    return {
-      body,
-      title,
-      tags,
-      tag,
-      error,
-      handleKeyDown,
-      handleSubmit,
-    };
-  },
+		return {
+			body,
+			title,
+			tags,
+			tag,
+			error,
+			handleKeyDown,
+			handleSubmit,
+		};
+	},
 };
 </script>
 
 <style>
 form {
-  max-width: 480px;
-  margin: 0 auto;
-  text-align: left;
+	max-width: 480px;
+	margin: 0 auto;
+	text-align: left;
 }
 input,
 textarea {
-  display: block;
-  margin: 10px 0;
-  width: 100%;
-  box-sizing: border-box;
-  padding: 10px;
-  border: 1px solid rgb(116, 114, 114);
+	display: block;
+	margin: 10px 0;
+	width: 100%;
+	box-sizing: border-box;
+	padding: 10px;
+	border: 1px solid rgb(116, 114, 114);
 }
 textarea {
-  height: 160px;
+	height: 160px;
 }
 label {
-  display: inline-block;
-  margin-top: 30px;
-  position: relative;
-  font-size: 20px;
-  color: white;
-  margin-bottom: 10px;
+	display: inline-block;
+	margin-top: 30px;
+	position: relative;
+	font-size: 20px;
+	color: white;
+	margin-bottom: 10px;
 }
 label::before {
-  content: "";
-  display: block;
-  width: 100%;
-  height: 100%;
-  background: #ff8800;
-  position: absolute;
-  z-index: -1;
-  padding-right: 40px;
-  left: -30px;
-  transform: rotateZ(-1.5deg);
+	content: "";
+	display: block;
+	width: 100%;
+	height: 100%;
+	background: #ff8800;
+	position: absolute;
+	z-index: -1;
+	padding-right: 40px;
+	left: -30px;
+	transform: rotateZ(-1.5deg);
 }
 button {
-  display: block;
-  margin-top: 30px;
-  background: #ff8800;
-  color: white;
-  border: none;
-  padding: 8px 16px;
-  font-size: 18px;
+	display: block;
+	margin-top: 30px;
+	background: #ff8800;
+	color: white;
+	border: none;
+	padding: 8px 16px;
+	font-size: 18px;
 }
 button:hover {
-  cursor: pointer;
-  opacity: 0.5;
+	cursor: pointer;
+	opacity: 0.5;
 }
 .pill {
-  display: inline-block;
-  margin: 10px 10px 0 0;
-  color: #444;
-  background: #ddd;
-  padding: 8px;
-  border-radius: 20px;
-  font-size: 14px;
+	display: inline-block;
+	margin: 10px 10px 0 0;
+	color: #444;
+	background: #ddd;
+	padding: 8px;
+	border-radius: 20px;
+	font-size: 14px;
 }
 </style>
